8eae3429ffbf68294c1fed1440eab66b8985d535
Christian McKinney <christian.mckinney@fdmgroup.com>
2024-06-06 00:43:05 -0500

    we added a new form to the admin page and updated the routes with the post request

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,4 +1,15 @@
 class AdminController < ApplicationController
     def index
     end
+
+    def create
+        #add to database
+    Rails.logger.info "Author: #{params[:author]}"
+    Rails.logger.info "Title: #{params[:title]}"
+    Rails.logger.info "Short Description: #{params[:short_description]}"
+    Rails.logger.info "Long Description: #{params[:long_description]}"
+    Rails.logger.info "YouTube Video ID: #{params[:youtube_video_id]}"
+    Rails.logger.info "Duration: #{params[:duration]}"
+    Rails.logger.info "Tags: #{params[:tags]}"
+    end
 end
